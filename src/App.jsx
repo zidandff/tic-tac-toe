f03d21d662f7be91c4f8aa491e3d1c266158aa5a
Zidan <zidandevs@gmail.com>
2025-02-06 19:33:32 +0700

    implement time travel

--- src/App.jsx
+++ src/App.jsx
@@ -8,61 +8,20 @@ function Cell({ value, onCellClick }) {
   );
 }
 
-function Board() {
-  const [cells, setCells] = useState(Array(9).fill(null));
-  const [xIsNext, setXisNext] = useState(true);
-
-  function handleClick(index) {
-    // jika cells sudah terisi keluar dari function untuk menghindari override cells
-    if (cells[index] || calculateWinner(cells)) return;
-
-    // duplikat array agar menerapkan immutability
-    const newCells = cells.slice();
-    newCells[index] = xIsNext ? 'x' : 'o';
-    setCells(newCells);
-    setXisNext(!xIsNext);
-  }
-
-  // cek apakah sudah ada pemenang
-  const winner = calculateWinner(cells);
-  console.log(winner);
-  let status;
-  if (winner) {
-    status = `Winner: ${winner}`;
-  } else {
-    status = `Next Player: ${xIsNext ? 'x' : 'o'}`;
-  }
-
-  // event handler untuk bermain lagi
-  function playAgainHandle() {
-    setCells(Array(9).fill(null));
-    if (winner === 'draw') {
-      setXisNext(xIsNext);
-    } else {
-      setXisNext(winner === 'x' ? true : false);
-    }
-  }
+function Board({ currentCells: cells, onPlay }) {
+  // const [cells, setCells] = useState(currentCells);
 
   return (
-    <div className="game-board">
-      <p className="status">{status}</p>
-      <div className="board">
-        <Cell value={cells[0]} onCellClick={() => handleClick(0)} />
-        <Cell value={cells[1]} onCellClick={() => handleClick(1)} />
-        <Cell value={cells[2]} onCellClick={() => handleClick(2)} />
-        <Cell value={cells[3]} onCellClick={() => handleClick(3)} />
-        <Cell value={cells[4]} onCellClick={() => handleClick(4)} />
-        <Cell value={cells[5]} onCellClick={() => handleClick(5)} />
-        <Cell value={cells[6]} onCellClick={() => handleClick(6)} />
-        <Cell value={cells[7]} onCellClick={() => handleClick(7)} />
-        <Cell value={cells[8]} onCellClick={() => handleClick(8)} />
-      </div>
-      <button
-        className={'play-again-btn ' + (winner ? 'show' : '')}
-        onClick={playAgainHandle}
-      >
-        Play Again
-      </button>
+    <div className="board">
+      <Cell value={cells[0]} onCellClick={() => onPlay(0)} />
+      <Cell value={cells[1]} onCellClick={() => onPlay(1)} />
+      <Cell value={cells[2]} onCellClick={() => onPlay(2)} />
+      <Cell value={cells[3]} onCellClick={() => onPlay(3)} />
+      <Cell value={cells[4]} onCellClick={() => onPlay(4)} />
+      <Cell value={cells[5]} onCellClick={() => onPlay(5)} />
+      <Cell value={cells[6]} onCellClick={() => onPlay(6)} />
+      <Cell value={cells[7]} onCellClick={() => onPlay(7)} />
+      <Cell value={cells[8]} onCellClick={() => onPlay(8)} />
     </div>
   );
 }
@@ -94,4 +53,87 @@ function calculateWinner(cells) {
   return null;
 }
 
-export default Board;
+function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [xIsNext, setXisNext] = useState(true);
+  const [currentCells, setCurrentCells] = useState(history[history.length - 1]);
+  // const currentCells = history[history.length - 1];
+
+  function handlePlay(index) {
+    // jika cells sudah terisi keluar dari function untuk menghindari override cells
+    if (currentCells[index] || calculateWinner(currentCells)) return;
+
+    // duplikat array agar menerapkan immutability
+    const newCells = currentCells.slice();
+    newCells[index] = xIsNext ? 'x' : 'o';
+
+    const newHistory = history.slice();
+    newHistory.push(newCells);
+
+    setHistory(newHistory);
+    setXisNext(!xIsNext);
+    setCurrentCells(newHistory[newHistory.length - 1]);
+
+    // console.log(newHistory);
+    // console.log(newHistory[newHistory.length - 1]);
+  }
+
+  // event handler untuk bermain lagi
+  function playAgainHandle() {
+    setHistory([Array(9).fill(null)]);
+    setCurrentCells(Array(9).fill(null));
+    if (winner === 'draw') {
+      setXisNext(xIsNext);
+    } else {
+      setXisNext(winner === 'x' ? true : false);
+    }
+  }
+
+  function backToHandle(index) {
+    console.log(history[index]);
+    setCurrentCells(history[index]);
+  }
+
+  // cek apakah sudah ada pemenang
+  const winner = calculateWinner(currentCells);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next Player: ${xIsNext ? 'x' : 'o'}`;
+  }
+
+  return (
+    <>
+      <div className="game">
+        <div>
+          <p className="status">{status}</p>
+          <Board
+            currentCells={currentCells}
+            xIsNext={xIsNext}
+            onPlay={handlePlay}
+          />
+          <button
+            className={'play-again-btn ' + (winner ? 'show' : '')}
+            onClick={playAgainHandle}
+          >
+            Play Again
+          </button>
+        </div>
+        <div className="game-history">
+          <ol>
+            {history.map((el, i) => (
+              <li key={i}>
+                <button onClick={() => backToHandle(i)}>
+                  Go to {i > 0 ? 'Move #' + i : 'game start'}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Game;
